fabf79c3ac9edcf3d5567d7ffbe9acad9da9bde2
zoro377 <roronovazoro377@gmail.com>
2026-01-01 16:56:19 +0530

    last commit

---
+++ fintrack-frontend/src/currency.ts
@@ -0,0 +1,8 @@
+export const formatINR = (amount: number) => {
+    return new Intl.NumberFormat("en-IN", {
+      style: "currency",
+      currency: "INR",
+      minimumFractionDigits: 2,
+    }).format(amount);
+  };
+  
